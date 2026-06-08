f5faf28f8e47730baf647e91450a29e93b7f5a27
Spud <10385195+yeSpud@users.noreply.github.com>
2026-06-08 17:37:51 -0600

    Moved icons getters to the custom icons interface

--- Spuds-Ski-Map/src/main/java/xyz/thespud/skimap/locationmanager/CustomIcons.kt
+++ Spuds-Ski-Map/src/main/java/xyz/thespud/skimap/locationmanager/CustomIcons.kt
@@ -1,7 +1,22 @@
 package xyz.thespud.skimap.locationmanager
 
+import xyz.thespud.skimap.R
+
 interface CustomIcons {
 
 	fun getOtherIcon(name: String): Int
+
+	fun getChairliftIcon(name: String): Int { return R.drawable.ic_chairlift }
+
+	fun getEasyIcon(): Int { return R.drawable.ic_green }
+
+	fun getMoreDifficultIcon(): Int { return R.drawable.ic_blue }
+
+	fun getMostDifficultIcon(): Int { return R.drawable.ic_black }
+
+	// TODO: Create icon for double black diamond runs
+	fun getExtremelyDifficultIcon(): Int { return R.drawable.ic_missing }
+
+	fun getTerrainParkIcon(): Int { return R.drawable.ic_terrain_park }
 
 }
